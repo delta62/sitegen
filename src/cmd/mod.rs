a597bef7852b944ca02401964ca9cb0830825b44
Sam Noedel <sam.noedel@gmail.com>
2023-02-07 21:54:46 -0800

    Enhance server MIME & directory handling

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::path::Path;
 use std::str::FromStr;
 
@@ -40,23 +40,42 @@ pub fn serve(args: &Args, config: &Config) -> Result<()> {
     let server = Server::http("localhost:8080").unwrap();
 
     for request in server.incoming_requests() {
+        log::info!("{} {}", request.method(), request.url());
+
         let path = &request.url()[1..];
         let path = Path::new(config.out_dir.as_str()).join(path);
-        log::info!("{} {:?}", request.method(), path);
+        let mut extension = path.extension().map(|e| e.to_string_lossy().to_string());
 
-        match File::open(path) {
+        let file = fs::metadata(path.as_path()).and_then(|metadata| {
+            if metadata.is_dir() {
+                let index = Path::new(path.as_path()).join("pages/index.html");
+                extension = index.extension().map(|e| e.to_string_lossy().to_string());
+                File::open(&index)
+            } else {
+                File::open(&path)
+            }
+        });
+
+        match file {
             Ok(file) => {
-                let response = Response::from_file(file)
-                    .with_header(Header::from_str("Content-Type: text/html").unwrap())
-                    .with_status_code(200);
-
-                request.respond(response).unwrap();
+                let content_type = if let Some(e) = extension {
+                    match e.as_str() {
+                        "html" => "text/html",
+                        "css" => "text/css",
+                        _ => "text/plain",
+                    }
+                } else {
+                    "text/html"
+                };
+                let content_type =
+                    Header::from_str(format!("Content-Type: {}", content_type).as_str()).unwrap();
+                let res = Response::from_file(file).with_header(content_type);
+                request.respond(res).unwrap();
             }
             Err(e) => {
                 log::error!("{:?}", e);
-                let response = Response::from_string(format!("{:?}", e)).with_status_code(500);
-
-                request.respond(response).unwrap();
+                let res = Response::from_string(format!("{:?}", e)).with_status_code(404);
+                request.respond(res).unwrap();
             }
         }
     }
