fb38aba76d8f08f9112ae4c2ee44fa404c5a1208
Sam Noedel <sam.noedel@gmail.com>
2023-02-11 20:35:29 -0800

    Add live reloading functionality

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -4,12 +4,19 @@ use crate::{
     args::Args,
     error::{Error, Result},
 };
-use futures_util::{future, StreamExt, TryStreamExt};
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
 use notify::{recommended_watcher, Event, EventKind, Watcher};
+use std::collections::HashMap;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 use tokio::fs;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::process::Command;
+use tokio_tungstenite::WebSocketStream;
+use tungstenite::Message;
+
+type WsWriter = SplitSink<WebSocketStream<TcpStream>, Message>;
+type Clients = Arc<Mutex<HashMap<String, WsWriter>>>;
 
 pub async fn clean(_args: &Args, config: &Config) -> Result<()> {
     fs::remove_dir_all(config.build.out_dir.as_str())
@@ -50,38 +57,31 @@ pub async fn build(_args: &Args, config: &Config) -> Result<()> {
         .await
 }
 
-fn rebuild<P: AsRef<Path>>(path: P, args: &Args, config: &Config) {
+fn rebuild<P: AsRef<Path>>(rt: &tokio::runtime::Runtime, path: P, args: &Args, config: &Config) {
     log::info!("{:?} changed", path.as_ref());
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap();
 
     rt.block_on(async {
         build(&args, &config).await.unwrap();
     });
 }
 
-async fn ws_listen() {
+async fn ws_listen(clients: Clients) {
     let addr = "localhost:8081";
     let listener = TcpListener::bind(addr).await.unwrap();
 
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
+        tokio::spawn(accept_connection(stream, clients.clone()));
     }
 }
 
-async fn accept_connection(stream: TcpStream) {
+async fn accept_connection(stream: TcpStream, clients: Clients) {
     let addr = stream.peer_addr().unwrap();
-    log::info!("Client {} connected", addr);
+    log::info!("{} connected", addr);
 
     let ws_stream = tokio_tungstenite::accept_async(stream).await.unwrap();
-    log::info!("WS connection: {}", addr);
-
-    let (write, read) = ws_stream.split();
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .unwrap();
+    let (writer, _) = ws_stream.split();
+    let mut guard = clients.lock().unwrap();
+    guard.insert(addr.to_string(), writer);
 }
 
 async fn http_listen(cmd: &str, args: &[String]) {
@@ -98,21 +98,40 @@ pub async fn serve(args: Args, config: Config) -> Result<()> {
     clean(&args, &config).await?;
     build(&args, &config).await?;
 
+    let ws_clients: Clients = Arc::new(Mutex::new(HashMap::new()));
     let local_config = config.clone();
     let http_server_cmd = local_config.http.command.as_str();
     let http_server_args = local_config.http.args.as_deref().unwrap_or_default();
 
+    let watcher_clients = ws_clients.clone();
     let mut watcher = recommended_watcher(move |res| match res {
         Ok(Event {
             kind: EventKind::Modify(_),
             paths,
             ..
-        }) => rebuild(paths.first().unwrap(), &args, &config),
-        Ok(Event {
+        })
+        | Ok(Event {
             kind: EventKind::Create(_),
             paths,
             ..
-        }) => rebuild(paths.first().unwrap(), &args, &config),
+        }) => {
+            let first_path = paths.first().unwrap();
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap();
+
+            rebuild(&rt, first_path, &args, &config);
+
+            let mut clients_guard = watcher_clients.lock().unwrap();
+            clients_guard.values_mut().for_each(|writer| {
+                rt.block_on(async {
+                    writer
+                        .send(Message::Text(String::from("reload")))
+                        .await
+                        .unwrap();
+                })
+            });
+        }
         _ => {}
     })
     .unwrap();
@@ -124,7 +143,7 @@ pub async fn serve(args: Args, config: Config) -> Result<()> {
     }
 
     // Start up a websocket server for live reloading
-    let ws = ws_listen();
+    let ws = ws_listen(ws_clients.clone());
     let http = http_listen(http_server_cmd, http_server_args);
 
     tokio::join!(ws, http);
